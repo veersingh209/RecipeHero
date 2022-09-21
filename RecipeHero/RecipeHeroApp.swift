fe0099610f4676aacaf6467193d8b905412d42b3
Veer Singh <veersingh209@icloud.com>
2022-09-21 16:07:13 -0700

    Initial Commit

---
+++ RecipeHero/RecipeHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeHeroApp.swift
+//  RecipeHero
+//
+//  Created by Veer Singh on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
